366e9dfb156fc8a9e8b2d2aef31e387dba4042c1
thaihoc1994 <bboyfool215@gmail.com>
2016-07-16 02:51:48 +0700

    First

--- FirstApplication/app/src/main/java/learn/hochnt/firstapplication/MainActivity.java
+++ FirstApplication/app/src/main/java/learn/hochnt/firstapplication/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Process layout
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//gan giao dien de xu ly
+        //R chinh la mot lop duoc tao ra tu android=>chua cac thanh phan trong du an (ID - Luu trong R)
+
     }
 }
